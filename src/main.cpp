8a35cc7a6d4f1d963dc853c039f7f6897a2c4853
arsimon1 <arsimon@students.chccs.k12.nc.us>
2023-11-19 21:26:33 -0500

    attempt number 2: triangle with legacy openGL

--- src/main.cpp
+++ src/main.cpp
@@ -1,33 +1,46 @@
+#define GL_SILENCE_DEPRECATION
+#include "GL/glew.h"
+#include "GLFW/glfw3.h"
 #include <iostream>
-#include "glad/glad.h"
-#include <GLFW/glfw3.h>
 
 int main() {
-    GLFWwindow *window;
     if (!glfwInit()) {
         return -1;
     }
 
-    window = glfwCreateWindow(640, 480, "Window!", NULL, NULL);
-    glfwMakeContextCurrent(window);
-
-    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
-        std::cout << "Couldn't load OpenGL" << std::endl;
+    GLFWwindow* window = glfwCreateWindow(800, 600, "OpenGL Window", nullptr, nullptr);
+    if (!window) {
         glfwTerminate();
         return -1;
     }
 
-    glClearColor(0.25f, 0.5f, 0.75f, 1.0f);
+    glfwMakeContextCurrent(window);
+
+    if (glewInit() != GLEW_OK) {
+        std::cout << "ERROR - Failed to initialize GLEW" << std::endl;
+    }
+
+    unsigned int a;
+    glGenBuffers(1, &a);
+
+
+
+
+
 
     while (!glfwWindowShouldClose(window)) {
-        glfwPollEvents();
-
         glClear(GL_COLOR_BUFFER_BIT);
+        glBegin(GL_TRIANGLES);
+        glVertex2f(-0.5f, -0.5f);
+        glVertex2f(0.0f, 0.5f);
+        glVertex2f(0.5f, -0.5f);
+        glEnd();
+        // Your rendering code goes here
 
         glfwSwapBuffers(window);
+        glfwPollEvents();
     }
 
     glfwTerminate();
     return 0;
-
 }
